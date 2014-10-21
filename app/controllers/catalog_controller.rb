31851c2a339f1ebb3c8135b0a6ffe49f0d413c7e
Vick <manuto-58@hotmail.com>
2014-10-20 22:58:37 -0500

    Catalogo
    Arreglo en presentación de catalogo

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,5 +1,7 @@
 class CatalogController < ApplicationController
   def index
   	@productsB = Product.where(:typeProduct => 'BIKE')
+  	@productsE = Product.where(:typeProduct => 'EQUIPMENT')
+  	@productsA = Product.where(:typeProduct => 'ACCESORY')
   end
 end
